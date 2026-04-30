9a33c6261f0e3237fea6ed7242cf7af34a0451e0
red-foler <qwertytr20905@gmail.com>
2026-04-30 03:02:03 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,3 +4,26 @@ function toggleCategory(element) {
     // Переключаем ему класс active
     parent.classList.toggle('active');
 }
+
+
+// 1. Находим нужные элементы
+const errorLabel = document.getElementById('skill-error');
+const allCheckboxes = document.querySelectorAll('.skill-group input[type="checkbox"]');
+
+// 2. Функция проверки
+function validateSkills() {
+    // Считаем, сколько галочек сейчас нажато
+    const checkedCount = document.querySelectorAll('.skill-group input[type="checkbox"]:checked').length;
+
+    if (checkedCount > 0) {
+        errorLabel.style.display = 'none'; // Прячем ошибку
+    } else {
+        errorLabel.style.display = 'block'; // Показываем ошибку
+    }
+}
+
+// 3. Вешаем "слушатель" на каждый чекбокс
+allCheckboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', validateSkills);
+});
+
