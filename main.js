5e329e7c64b83877dbbf44a2ab7a97880d17d5ac
red-foler <qwertytr20905@gmail.com>
2026-05-01 01:30:46 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -16,14 +16,49 @@ function validateSkills() {
     const checkedCount = document.querySelectorAll('.skill-group input[type="checkbox"]:checked').length;
 
     if (checkedCount > 0) {
-        errorLabel.style.display = 'none'; // Прячем ошибку
+        errorLabel.style.color = '#00000000'; // Прячем ошибку
+        errorLabel.style.fontSize = '10px';
+        errorLabel.style.lineHeight = '5px';
+        clearBtn.style.opacity = '1';
+        clearBtn.style.fontSize = '18px';
+        clearBtn.style.height = '30px';
+        clearBtn.style.margin = '8px 0';
+        
     } else {
-        errorLabel.style.display = 'block'; // Показываем ошибку
+        errorLabel.style.color = 'red'; // Показываем ошибку
+        errorLabel.style.fontSize = '13px';
+        errorLabel.style.lineHeight ='20px';
+        clearBtn.style.opacity = '0';
+        clearBtn.style.height = '0px';
+        clearBtn.style.fontSize = '10px';
+        clearBtn.style.margin = '0';
     }
 }
 
 // 3. Вешаем "слушатель" на каждый чекбокс
 allCheckboxes.forEach(checkbox => {
     checkbox.addEventListener('change', validateSkills);
+});
+
+
+
+
+
+const clearBtn = document.getElementById('clear-all');
+clearBtn.addEventListener('click', () => {
+    // Находим ВСЕ чекбоксы
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach(cb => {
+        cb.checked = false; // Снимаем галочку
+        errorLabel.style.color = 'red'; // Показываем ошибку
+        errorLabel.style.fontSize = '13px';
+        errorLabel.style.lineHeight ='20px';
+        clearBtn.style.opacity = '0';
+        clearBtn.style.height = '1px';
+        clearBtn.style.fontSize = '10px';
+        clearBtn.style.margin = '0';
+    });
+    // После сброса нужно обновить локальное хранилище
+    
 });
 
